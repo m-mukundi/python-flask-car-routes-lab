cf78a8a73ff055beb9b93d171c5c490f381f9ef1
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-07-27 15:13:33 +0300

    Routing Basics Completed

--- server/app.py
+++ server/app.py
@@ -1,1 +1,22 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+from flask import Flask
+
+app = Flask(__name__)
+
+existing_models = ["Beedle", "Crossroads", "M2", "Panique"]
+
+
+@app.route("/")
+def index():
+    return "<h1>Welcome to Flatiron Cars</h1>"
+
+
+@app.route("/<string:model>")
+def request_a_model(model):
+    if model not in existing_models:
+        return f"<h1>No models called {model} exists in our catalog</h1>"
+
+    return f"<h1>Flatiron {model} is in our fleet!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
