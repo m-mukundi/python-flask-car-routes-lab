2d198e9401fc42c753f65ddf1a3bf64d03de6678
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-07-27 16:32:51 +0300

    Lab complete with all tests passing

--- server/app.py
+++ server/app.py
@@ -7,15 +7,15 @@ existing_models = ["Beedle", "Crossroads", "M2", "Panique"]
 
 @app.route("/")
 def index():
-    return "<h1>Welcome to Flatiron Cars</h1>"
+    return "Welcome to Flatiron Cars"
 
 
 @app.route("/<string:model>")
 def request_a_model(model):
     if model not in existing_models:
-        return f"<h1>No models called {model} exists in our catalog</h1>"
+        return f"No models called {model} exists in our catalog"
 
-    return f"<h1>Flatiron {model} is in our fleet!</h1>"
+    return f"Flatiron {model} is in our fleet!"
 
 
 if __name__ == "__main__":
